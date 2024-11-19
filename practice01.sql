7cd8af8e263d6793c90014fb8de6f4d2ae18a932
BLT24Q4 <blt24q4@gmail.com>
2024-11-19 10:53:15 +0900

    Update practice01.sql

--- practice01.sql
+++ practice01.sql
@@ -58,7 +58,76 @@ ORDER BY max_salary DESC;
 
 /*
 문제5.
-월급이 14000 미만 10000 이상인 직원의 이름(first_name), 월급, 커미션퍼센트 를 월급순(내림차순) 출력하세오. 단 커미션퍼센트 가 null 이면 0 으로 나타내시오
-문제6.
-부서번호가
+월급이 14000 미만 10000 이상인 
+
+직원의 이름(first_name), 월급, 커미션퍼센트 를 
+
+월급순(내림차순) 출력하세오. 
+
+단 커미션퍼센트 가 null 이면 0 으로 나타내시오
 */
+SELECT first_name,
+	salary,
+    commission_pct,
+    IFNULL(commission_pct, 0)
+FROM employees
+WHERE salary < 14000 AND
+	salary >= 10000
+ORDER BY
+	salary DESC;
+    
+/*
+문제6.
+부서번호가 10, 90, 100 인 
+직원의 이름, 월급, 입사일, 부서번호를 나타내시오
+입사일은 1977-12 와 같이 표시하시오
+*/
+SELECT
+	first_name, 
+    salary, 
+    DATE_FORMAT(hire_date, '%Y-%m') AS hire_month, 
+    department_id
+FROM employees
+WHERE department_id = 10 OR
+	department_id = 90 OR
+    department_id = 100;
+    
+-- 아니면,
+
+SELECT
+	first_name, 
+    salary, 
+    DATE_FORMAT(hire_date, '%Y-%m') AS hire_month, 
+    department_id
+FROM employees
+WHERE department_id IN (10, 90, 100);
+
+/*
+문제7.
+이름(first_name)에 S 또는 s 가 들어가는 직원의 
+
+이름, 월급을 나타내시오
+*/
+SELECT first_name, salary
+FROM employees
+WHERE UPPER(first_name) LIKE '%S%';
+
+/*
+문제8.
+전체 부서를 출력하려고 합니다. 
+순서는 부서이름이 긴 순서대로 출력해 보세요.
+*/
+SELECT *, LENGTH(department_name) FROM departments
+ORDER BY LENGTH(department_name) DESC;
+
+/*
+문제9.
+정확하지 않지만, 지사가 있을 것으로 예상되는 나라들을 
+
+나라이름을 대문자로 출력하고
+
+오름차순(ASC)으로 정렬해 보세요.
+*/
+SELECT UPPER(country_name) AS country_name
+FROM countries
+ORDER BY UPPER(county_name) ASC;
