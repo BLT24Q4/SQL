75ed6d2472be37eca7a141f3ec744a0db14f4d22
BLT24Q4 <blt24q4@gmail.com>
2024-11-19 17:41:44 +0900

    Update practice03.sql

--- practice03.sql
+++ practice03.sql
@@ -143,27 +143,70 @@ ORDER BY cou.country_name;
 문제7.
 job_history
 테이블은 과거의 담당업무의 데이터를 가지고 있다
-과거의 업무아이디 (job_id)가 'AC_ACCOUNT'로 근무한 사원의 사번, 이름(풀네임), 업무아이디, 시작일, 종료일을 출력하세요
+과거의 업무아이디 (job_id)가 'AC_ACCOUNT'로 근무한 사원의 
+사번, 이름(풀네임), 
+업무아이디, 시작일, 종료일을 출력하세요
 이름은 first_name과 last_name을 합쳐 출력합니다
 (2건)
 */
+SELECT emp.employee_id 사번,
+	CONCAT(first_name, ' ', last_name) 이름,
+    jh.job_id 업무아이디,
+    jh.start_date 시작일,
+    jh.end_date 종료일
+FROM employees emp
+	INNER JOIN job_history jh
+		ON emp.employee_id = jh.employee_id
+WHERE jh.job_id = 'AC_ACCOUNT';
+
 
 /*
 문제8.
-각 부서 (department)에 대해서 부서번호 (department_id), 부서이름 (department_name), 
-	매니저(manager)의 이름 (first_name), 위치 (locations) 한 도시 (city), 나라 (countries) 의 이름
-	(countries_name) 그리고 지역구분 (regions) 의 이름 (region_name) 까지 전부 출력해 보세요
+각 부서 (department)에 대해서 
+	부서번호 (department_id), 부서이름 (department_name), 
+	매니저(manager)의 이름 (first_name), 
+    위치 (locations) 한 도시 (city), 
+    나라 (countries) 의 이름 (country_name) 그리고 
+    지역구분 (regions) 의 이름 (region_name) 까지 전부 출력해 보세요
 (11건)
 */
+SELECT dept.department_id 부서번호,
+		dept.department_name 부서이름,
+        emp.first_name 매니저이름,
+        loc.city 위치한도시,
+        cou.country_name 나라이름,
+        reg.region_name 지역명
+FROM departments dept,
+	employees emp,
+    locations loc,
+    countries cou,
+    regions reg
+WHERE dept.manager_id = emp.employee_id AND
+	dept.location_id = loc.location_id AND
+    loc.country_id = cou.country_id AND
+    cou.region_id = reg.region_id
+ORDER BY dept.department_id ASC;
+
 
 /*
 문제9.
-각 사원 (employee)에 대해서 사번 (employee_id), 이름 (first_name), 부서명(department_name), 
+각 사원 (employee)에 대해서 
+	사번 (employee_id), 이름 (first_name), 
+    부서명(department_name), 
 	매니저 (manager)의 이름 (first_name) 을 조회하세요
-부서가 없는 직원 Kimberely)도 표시합니다
+부서가 없는 직원 (Kimberely)도 표시합니다
 매니저가 없는 Steven 도 표시합니다
 (107 명)
 */
+SELECT emp.employee_id 사번,
+		emp.first_name 이름,
+        dept.department_name 부서명,
+        man.first_name 매니저이름
+FROM employees emp
+	LEFT JOIN departments dept
+		ON emp.department_id = dept.department_id
+	LEFT JOIN employees man
+		ON emp.manager_id = man.employee_id;
 
 /*
 문제9-1.
@@ -171,10 +214,28 @@ job_history
 매니저가 없는 Steven은 표시하지 않습니다
 (106 명)
 */
-
+SELECT emp.employee_id 사번,
+		emp.first_name 이름,
+        dept.department_name 부서명,
+        man.first_name 매니저이름
+FROM employees emp
+	LEFT JOIN departments dept
+		ON emp.department_id = dept.department_id
+	INNER JOIN employees man
+		ON emp.manager_id = man.employee_id;
+        
 /*
 문제9-2.
 문제9 에서 부서가 없는 직원 (Kimberly) 도 표시 하지 않고
 매니저가 없는 Steven 도 표시하지 않습니다
 (105 명)
 */
+SELECT emp.employee_id 사번,
+		emp.first_name 이름,
+        dept.department_name 부서명,
+        man.first_name 매니저이름
+FROM employees emp
+	INNER JOIN departments dept
+		ON emp.department_id = dept.department_id
+	INNER JOIN employees man
+		ON emp.manager_id = man.employee_id;
