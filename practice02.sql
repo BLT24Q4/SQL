5681c84427fdea12f2f16ed38d8e1c01a9014018
BLT24Q4 <blt24q4@gmail.com>
2024-11-20 15:20:21 +0900

    Update practice02.sql

--- practice02.sql
+++ practice02.sql
@@ -4,6 +4,13 @@
 매니저가있는 직원은 몇 명입니까 ? 아래의 결과가 나오도록 쿼리문을 작성하세요
 (106명)
 */
+SELECT COUNT(*) haveMngCnt
+FROM employees 
+WHERE manager_id IS NOT NULL;
+
+-- 집계함수는 NULL을 제외하고 집계
+SELECT COUNT(manager_id) haveMngCnt
+FROM employees;
 
 
 /*
@@ -12,13 +19,19 @@
 최고임금(salary)과 최저임금을 최고임금 , 최저임금 으로 출력해 보세요.
 두 임금의 차이는 얼마인가요 ? "최고임금-최저임금"이란 타이틀로 함께 출력해 보세요
 */
-
+SELECT MAX(salary) 최고임금,
+		MIN(salary) 최저임금,
+        MAX(salary) - MIN(salary) "최고임금-최저임금"
+FROM employees;
 
 /*
 문제3.
 마지막으로 신입사원이 들어온 날은 언제 입니까 ? 다음 형식으로 출력해주세요
 예) 2014년 07월 10일
 */
+
+SELECT DATE_FORMAT(MAX(hire_date), "%Y년 %m월 %d일")
+FROM employees;
 
 
 /*
